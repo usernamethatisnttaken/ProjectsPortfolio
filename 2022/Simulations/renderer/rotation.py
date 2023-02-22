78f5b0b1a2c3ce3c45a873b739e81b6149ab375c
usernamethatisnttaken <hjm2308@g.rit.edu>
2023-02-22 15:36:41 -0500

    rotation refactoring done

--- 2022/Simulations/renderer/rotation.py
+++ 2022/Simulations/renderer/rotation.py
@@ -3,32 +3,41 @@ import math
 
 class rotation():
     def __init__(self):
-        self.thetax = 0
-        self.thetay = 0
-        self.thetaz = 0
+        self.__thetax = 0
+        self.__thetay = 0
+        self.__thetaz = 0
+        self.__speed  = 2
+
+    def __mod_up(self, n):
+        return (n + math.radians(self.__speed)) % math.radians(360)
+    
+    def __mod_down(self, n):
+        return (n - math.radians(self.__speed) + math.radians(360)) % math.radians(360)
 
     def move(self):
-        dist = 2
         keys = pygame.key.get_pressed()
         if keys[pygame.K_UP]:
-            self.thetax = (self.thetax - math.radians(dist) + math.radians(360)) % math.radians(360)
+            self.__thetax = self.__mod_down(self.__thetax)
         elif keys[pygame.K_DOWN]:
-            self.thetax = (self.thetax + math.radians(dist)) % math.radians(360)
+            self.__thetax = self.__mod_up(self.__thetax)
         if keys[pygame.K_a]:
-            self.thetay = (self.thetay - math.radians(dist) + math.radians(360)) % math.radians(360)
+            self.__thetay = self.__mod_down(self.__thetay)
         elif keys[pygame.K_d]:
-            self.thetay = (self.thetay + math.radians(dist)) % math.radians(360)
+            self.__thetay = self.__mod_up(self.__thetay)
         if keys[pygame.K_LEFT]:
-            self.thetaz = (self.thetaz - math.radians(dist) + math.radians(360)) % math.radians(360)
+            self.__thetaz = self.__mod_down(self.__thetaz)
         elif keys[pygame.K_RIGHT]:
-            self.thetaz = (self.thetaz + math.radians(dist)) % math.radians(360)
+            self.__thetaz = self.__mod_up(self.__thetaz)
 
     def calc(self, table):
-        new_table = []
+        new_tbl = []
+        x = self.__thetax
+        y = self.__thetay
+        z = self.__thetaz
         for point in table:
-            temp = [0, 0, 0]
-            temp[0] = (math.cos(self.thetax) * math.cos(self.thetay) * point[0]) + (math.cos(self.thetax) * math.sin(self.thetay) * math.sin(self.thetaz) - math.sin(self.thetax) * math.cos(self.thetaz)) * point[1] + (math.cos(self.thetax) * math.sin(self.thetay) * math.cos(self.thetaz) + math.sin(self.thetax) * math.sin(self.thetaz)) * point[2]
-            temp[1] = (math.sin(self.thetax) * math.cos(self.thetay) * point[0]) + (math.sin(self.thetax) * math.sin(self.thetay) * math.sin(self.thetaz) + math.cos(self.thetax) * math.cos(self.thetaz)) * point[1] + (math.sin(self.thetax) * math.sin(self.thetay) * math.cos(self.thetaz) - math.cos(self.thetax) * math.sin(self.thetaz)) * point[2]
-            temp[2] = (-math.sin(self.thetay) * point[0]) + (math.cos(self.thetay) * math.sin(self.thetaz) * point[1]) + (math.cos(self.thetay) * math.cos(self.thetaz) * point[2])
-            new_table.append(temp)
-        return new_table
+            temp = []
+            temp.append((math.cos(x) * math.cos(y) * point[0]) + (math.cos(x) * math.sin(y) * math.sin(z) - math.sin(x) * math.cos(z)) * point[1] + (math.cos(x) * math.sin(y) * math.cos(z) + math.sin(x) * math.sin(z)) * point[2])
+            temp.append((math.sin(x) * math.cos(y) * point[0]) + (math.sin(x) * math.sin(y) * math.sin(z) + math.cos(x) * math.cos(z)) * point[1] + (math.sin(x) * math.sin(y) * math.cos(z) - math.cos(x) * math.sin(z)) * point[2])
+            temp.append((-math.sin(y) * point[0]) + (math.cos(y) * math.sin(z) * point[1]) + (math.cos(y) * math.cos(z) * point[2]))
+            new_tbl.append(temp)
+        return new_tbl
